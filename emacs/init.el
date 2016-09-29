97dd1e445ddd500a50bafc8c27b61d114af659c9
Matthew Hill <matthew.jh@outlook.com>
2016-09-29 21:29:20 +0100

    adds slime

--- emacs/init.el
+++ emacs/init.el
@@ -1,7 +1,9 @@
 (let ((default-directory (file-name-directory load-file-name)))
   (add-to-list 'load-path (file-truename "vendor"))
   (add-to-list 'load-path (file-truename "vendor/slime-2.18"))
-  (load-file (file-truename "files/key-bindings.el")))
+  (load-file (file-truename "files/key-bindings.el"))
+  (setq common-lisp-hyperspec-root (file-truename "../lisp/HyperSpec/")
+        common-lisp-hyperspec-symbol-table (file-truename "../lisp/HyperSpec/Data/Map_Sym.txt")))
 
 ;; general emacs
 
@@ -159,8 +161,18 @@
   '(ace-jump-mode-enable-mark-sync))
 
 ;; slime
-(require 'slime-autoloads)
-(setq inferior-lisp-program "clisp")
+(require 'slime)
+(slime-setup '(slime-fancy)) 
+(add-hook 'lisp-mode-hook (lambda () (slime-mode t)))
+(add-hook 'inferior-lisp-mode-hook (lambda () (inferior-slime-mode t)))
+(setq inferior-lisp-program "clisp -K full")
+
+(require 'ac-slime)
+(add-hook 'slime-mode-hook 'set-up-slime-ac)
+(add-hook 'slime-repl-mode-hook 'set-up-slime-ac)
+(eval-after-load "auto-complete"
+  '(add-to-list 'ac-modes 'slime-repl-mode))
+
 
 ;; melpa
 
